6a5f85eb42b338718190e2ef7e236c07db3c271c
Nicholas Won <nlw55@cornell.edu>
2023-05-07 22:19:04 -0400

    working on subpages

--- pages/dining-hall/[id].tsx
+++ pages/dining-hall/[id].tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-import ReviewCard from '../../components/ReviewCard';
-import styles from '../../styles/dining-hall.module.css';
 import { db } from "../../util/firebase"
 import { onSnapshot, collection, query} from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { ReviewWithId, Review } from "../../types"
-import { Heading, Spinner, VStack } from "@chakra-ui/react"
+import { Spinner, VStack } from "@chakra-ui/react"
 import ReviewList from "../../components/ReviewList"
 import AddControl from "../../components/AddControl"
 
@@ -50,24 +47,16 @@ const diningHalls = [
     }
 ];
 
-// hardcoded reviews, delete later...
-const diningHallReviews: { [key: string]: { userID: string; rating: number; description: string; }[] } = {
-    '1': [
-        { userID: 'user1', rating: 5, description: 'Great food!' },
-        { userID: 'user2', rating: 3, description: 'It was okay.' },
-    ],
-    '2': [
-        { userID: 'user3', rating: 4, description: 'Nice place!' },
-        { userID: 'user4', rating: 5, description: 'Awesome food!' },
-    ],
-    '3': [
-        { userID: 'user5', rating: 2, description: 'Not great.' },
-        { userID: 'user6', rating: 3, description: 'Could be better.' },
-    ],
+// query based on location (still need to implement)
+let scores = {
+    '1': "morrison", 
+    '2': "northstar", 
+    '3': "risley", 
+    '4': "okenshields", 
+    '5': "terrace", 
+    '6': "trillium"
 };
-
-// query based on location (setup conditional later)
-const reviewQuery = query(collection(db, "morrison"));
+const reviewQuery = query(collection(db, "morrison"))
 
 const FetchReviews = () => {
     const [reviews, setReviews] = useState<ReviewWithId[] | null>(null)
@@ -90,37 +79,3 @@ const FetchReviews = () => {
 }
 
 export default FetchReviews
-
-// location-specific subpages
-/*
-export default function DiningHall() {
-    const router = useRouter();
-    const id = String(router.query.id);
-
-    if (!id) {
-        return null;
-    }
-
-    const diningHall = diningHalls.find((hall) => hall.id === id);
-
-    if (!diningHall) {
-        return <div>Dining hall not found.</div>;
-    }
-
-    const reviews = diningHallReviews[id] || [];
-
-    return (
-        <div>
-            <h1>{diningHall.name}</h1>
-            <img src={diningHall.imageURL} alt={diningHall.name} />
-            <p>Average Rating: {diningHall.avgRating}</p>
-
-            <div className={styles.reviews}>
-                {reviews.map((review, index) => (
-                    <ReviewCard key={index} review={review} />
-                ))}
-            </div>
-        </div>
-    );
-}
-*/
